5bf406d2ffc30064e55dea9bc547746515ed7369
Anderson Belderrama <andersonbelderrama@gmail.com>
2024-05-14 17:09:59 -0300

    theme v1 and start transactions page

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,7 +2,7 @@
       <v-layout>
             <v-app-bar :color="$vuetify.theme.global.current.dark ? 'grey-darken-4' : 'grey-lighten-4'" class="py-1">
                   <template v-slot:prepend class="d-flex p-2">
-                        <v-app-bar-nav-icon @click.stop="drawer = !drawer" ></v-app-bar-nav-icon>
+                        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
                         <div class="px-6">
                               <a href="https://vuetifyjs.com" rel="noopener noreferrer" target="_blank">
                                     <Logo width="130" />
@@ -18,12 +18,24 @@
 
             </v-app-bar>
 
-            <v-navigation-drawer :color="$vuetify.theme.global.current.dark ? 'deep-purple-darken-2' : 'deep-purple-darken-1'" v-model="drawer">
-
+            <v-navigation-drawer
+                  :color="$vuetify.theme.global.current.dark ? 'deep-purple-darken-2' : 'deep-purple-darken-1'"
+                  v-model="drawer">
+                  <v-divider></v-divider>
+                  <v-list>
+                        <v-list-subheader color="deep-purple-lighten-5">MENU PRINCIPAL</v-list-subheader>
+                        <v-list-item v-for="item in items" :key="item.title" :title="item.title" :to="item.to"
+                              :prepend-icon="item.icon" :active="$route.path === item.to"></v-list-item>
+                  </v-list>
             </v-navigation-drawer>
 
             <v-main>
-                  <router-view />
+                  <v-container grid-list-xs>
+                        <h1>{{ $route.meta.title }}</h1>
+                        <v-divider></v-divider>
+                        <router-view />
+                  </v-container>
+
             </v-main>
       </v-layout>
 </template>
@@ -38,7 +50,12 @@ export default {
             Logo
       },
       data: () => ({
-            drawer: true
+            drawer: true,
+            items: [
+                  { title: 'Dashboard', icon: 'mdi:mdi-view-dashboard', to: '/' },
+                  { title: 'Contas', icon: 'mdi:mdi-account-box', to: '/about' },
+                  { title: 'Transações', icon: 'mdi:mdi-gavel', to: '/transactions' },
+            ],
 
       }),
       computed: {
---
+++ src/pages/transactions.vue
@@ -0,0 +1,16 @@
+<template>
+      <div class="d-flex py-2">
+            Test
+      </div>
+</template>
+
+<route lang="yaml">
+  meta:
+    layout: default
+    requiresAuth: true
+    title: Transações
+</route>
+
+<script>
+
+</script>
